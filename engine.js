8c2fb3410ade5651d0943a64c2ef98eed126e5cd
mullere <edward.j.muller@gmail.com>
2016-06-02 17:42:43 -0500

    Update engine.js

--- engine.js
+++ engine.js
@@ -1,5 +1,5 @@
 var fps = 50, mouseClickX = 0, mouseClickY = 0, mousePosX = 0, mousePosY = 0, selected=null, oldSelected=null;
-var entities = []; var events = []; var mouseEvents = []; var motionBehaviors = [];
+var entities = []; var events = []; var collisionEvents = []; var mouseEvents = []; var motionBehaviors = [];
 function gamespace(){
 	this.canvas = document.getElementById('gamespace');
 	this.initialize = function(){
@@ -11,6 +11,7 @@ function gamespace(){
 }
 function gamestate(){
 	this.updatestate = function(){
+		checkCollisions();
 		runmotionBehavior();
 		emptyEventQueue();
 	};
@@ -58,7 +59,7 @@ function drawEntities(){
 		ctx.fill();	
 	}
 }
-function entity(name,type,height,width,color,posX,posY,speedX,speedY,targetX,targetY,collision){
+function entity(name,type,height,width,color,posX,posY,speedX,speedY,targetX,targetY){
 	var newEntity = {
 		'name':name,
 		'type':type,
@@ -71,7 +72,6 @@ function entity(name,type,height,width,color,posX,posY,speedX,speedY,targetX,tar
 		'speedY':speedY,
 		'targetX':targetX,
 		'targetY':targetY,
-		'collision':collision,
 	};
 	entities.push(newEntity);
 }
@@ -116,6 +116,44 @@ function mouseListener(){
 function keyPressListener(action){
 
 }
+function flightMode1(entity){
+	entity.speedX = 2.5*(entity.targetX - entity.posX)/gamespace.canvas.width;
+	entity.speedY = 2.5*(entity.targetY - entity.posY)/gamespace.canvas.width;
+}
+function flightMode2(entity){
+	diffX = entity.targetX - entity.posX;
+	diffY = entity.targetY - entity.posY;
+	entity.speedX = -Math.cos(Math.atan(diffY/diffX));
+	entity.speedY = -Math.sin(Math.atan(diffY/diffX));
+}
+function overlap(entity1,entity2){
+	//console.log('working');
+	if(entity1.type==entity2.type){
+		return false;
+	}
+	radius1 = entity1.width/2;
+	radius2 = entity2.width/2;
+	distance = Math.sqrt(Math.pow(entity1.posX-entity2.posX,2)+Math.pow(entity1.posY-entity2.posY,2));
+	if(distance<=radius1+radius2){
+		return true;
+	} else {
+		return false;
+	}
+}
+function checkCollisions(){
+	for(i=0;i<entities.length;i++){
+		for(j=0;j<entities.length;j++){
+			if(overlap(entities[i],entities[j])){
+				for(n=0;n<collisionEvents.length;n++){
+					collisionEvents[n](entities[i],entities[j]);
+				}
+			}
+		}
+	}
+}
+function collisionEvent(event){
+	collisionEvents.push(event);
+}
 gamestate = new gamestate();
 gamestate.start();
 
@@ -125,48 +163,32 @@ gamestate.start();
 // this is where the game engine ends and the components that make up the game are added.
 var playableColor = 'rgba(0,110,110,1)';
 var playableHighlighted = 'rgba(0,150,150,1)';
+var playableDark = 'rgba(0,70,70,1)';
 var cloudColor = 'rgba(130,130,130,0.5)';
 var enemyColor = 'rgba(140,0,50,1)';
 new mouseListener();
-new entity('playable1','pc',50,50,playableColor,400,400,0,0,400,400,true);
-new entity('playable2','pc',50,50,playableColor,350,350,0,0,350,350,true);
-new entity('enemy1','npc',50,50,enemyColor,100,100,0,0,100,100,true);
-new entity('enemy2','npc',50,50,enemyColor,150,150,0,0,150,150,true);
-new entity('cloud','env',100,100,cloudColor,250,250,0,0,250,250,true);
-new entity('cloud','env',100,100,cloudColor,200,300,0,0,200,300,true);
-new entity('cloud','env',100,100,cloudColor,300,200,0,0,300,200,true);
-function hurryUpAndWait(){
-	for(i=0;i<entities.length;i++){
-		//if((entities[i].type=='pc')||(entities[i].type=='npc')){
-			entities[i].speedX = 2.5*(entities[i].targetX - entities[i].posX)/gamespace.canvas.width;
-			entities[i].speedY = 2.5*(entities[i].targetY - entities[i].posY)/gamespace.canvas.height;
-		//} else {
-
-		//}
-		entities[i].posX += entities[i].speedX;
-		entities[i].posY += entities[i].speedY;
-	}
-}
-/*function slowAndSteady(){
-	roomForError = 2;
-	for(i=0;i<entities.length;i++){
-
-		if((entities[i].posX-entities[i].targetX)>roomForError){
-			entities[i].speedX = 0;
-		} else {
-			entities[i].speedX = 0;
-		}
-		if((entities[i].posY-entities[i].targetY)>roomForError){
-			entities[i].speedY = 0;
-		} else {
-			entities[i].speedY = 0;
-		}
-		entities[i].posX += entities[i].speedX;
-		entities[i].posY += entities[i].speedY;
-	}
-}*/
+new entity('playable1','pc',50,50,playableColor,400,400,0,0,400,400);
+new entity('playable2','pc',50,50,playableColor,350,350,0,0,350,350);
+new entity('enemy1','npc',50,50,enemyColor,100,100,0,0,100,100);
+new entity('enemy2','npc',50,50,enemyColor,150,150,0,0,150,150);
+new entity('cloud','env',100,100,cloudColor,250,250,0,0,250,250);
+new entity('cloud','env',100,100,cloudColor,200,300,0,0,200,300);
+new entity('cloud','env',100,100,cloudColor,300,200,0,0,300,200);
 new motionBehavior(function(){
-	hurryUpAndWait();
+	for(i=0;i<entities.length;i++){
+		if((entities[i].type=='pc')||(entities[i].type=='npc')){
+			flightMode1(entities[i]);
+		} else if(entities[i].type=='mun') {
+			flightMode2(entities[i]);
+		}
+		entities[i].posX += entities[i].speedX;
+		entities[i].posY += entities[i].speedY;
+	}
+});
+new collisionEvent(function(entity1,entity2){
+	if((entity1.type=='npc')&&(entity2.type=='mun')){
+		console.log('boom');
+	}
 });
 new mouseEvent(function(){
 	// case where selecting 'pc' entity for the first time
@@ -177,17 +199,30 @@ new mouseEvent(function(){
 	}
 });
 new mouseEvent(function(){
-	// case where selecting nullspace or another entity after selecting 'pc' entity
+	// selection events
 	if(oldSelected!=null){
 		if(entities[oldSelected].type=='pc'){
-			if((selected==null)||(entities[selected].type=='env')){
+			if(selected==null){
+				// case where selecting nullspace
 				entities[oldSelected].color = playableColor;
 				entities[oldSelected].targetX = mouseClickX;
 				entities[oldSelected].targetY = mouseClickY;
-			} else if(entities[selected].type=='npc'){
-				// here there be weapon fire
-				new entity('cannonfire','mun',10,10,'yellow',entities[oldSelected].posX,entities[oldSelected].posY,0,0,entities[selected].posX,entities[selected].posY,true);
-				selected=oldSelected;
+			} else {
+				if(entities[selected].type=='env'){
+					// case where selecting 'env'
+					entities[oldSelected].color = playableColor;
+					entities[oldSelected].targetX = mouseClickX;
+					entities[oldSelected].targetY = mouseClickY;
+				} else if(entities[selected].type=='pc'){
+					// case where selecting another 'pc'
+					if(selected!=oldSelected){
+						entities[oldSelected].color = playableColor;
+					}
+				} else if(entities[selected].type=='npc'){
+					// case where selecting 'npc'
+					new entity('cannonfire','mun',10,10,'yellow',entities[oldSelected].posX,entities[oldSelected].posY,0,0,entities[selected].posX,entities[selected].posY);
+					selected=oldSelected;
+				}
 			}
 		}
 	}
